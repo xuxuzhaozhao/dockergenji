ccaaaf6bf308fa08ad242f711a893486af2f2529
xuchengyi <xuchengyi999@gmail.com>
2019-04-20 16:51:29 +0800

    init docker genji project

---
+++ GenJii/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GenJii.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
