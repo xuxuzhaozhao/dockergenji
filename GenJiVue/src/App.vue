9e10cbeb745aaa174db4f581c57980c9ac4d0aa6
xuchengyi <xuchengyi999@gmail.com>
2019-05-22 10:42:08 +0800

    Simplifed Project

---
+++ GenJiVue/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>XuChengyi</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+    <div class="person-info">
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          height="200px"
+        >
+        </v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">Top western road trips</div>
+            <span class="grey--text">1,000 miles of wonder</span>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat>Share</v-btn>
+          <v-btn flat color="purple">Explore</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn icon @click="show = !show">
+            <v-icon>{{
+              show ? "keyboard_arrow_down" : "keyboard_arrow_up"
+            }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-slide-y-transition>
+          <v-card-text v-show="show">
+            I'm a thing. But, like most politicians, he promised more than he
+            could deliver. You won't have time for sleeping, soldier, not with
+            all the bed making you'll be doing. Then we'll go with that data
+            file! Hey, you add a one and two zeros to that or we walk! You're
+            going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </v-slide-y-transition>
+      </v-card>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.person-info {
+  position: fixed;
+  left: 65%;
+  top: 87px;
+}
+</style>
